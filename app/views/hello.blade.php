ae3d9a034f3976db7910d890118fa0b5ec103320
jaimeceballos <jaimeceballos82@gmail.com>
2014-11-17 01:04:09 -0300

    public page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -7,78 +7,7 @@
 </head>
 
 <body>
-	<div class="col-md-12 column" style="padding-bottom:50px;">
-			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-				<div class="navbar-header">
-					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
-				</div>
-				
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="#">Link</a>
-						</li>
-						<li>
-							<a href="#">Link</a>
-						</li>
-						<li class="dropdown">
-							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#">Action</a>
-								</li>
-								<li>
-									<a href="#">Another action</a>
-								</li>
-								<li>
-									<a href="#">Something else here</a>
-								</li>
-								<li class="divider">
-								</li>
-								<li>
-									<a href="#">Separated link</a>
-								</li>
-								<li class="divider">
-								</li>
-								<li>
-									<a href="#">One more separated link</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" />
-						</div> <button type="submit" class="btn btn-default">Submit</button>
-					</form>
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="#">Link</a>
-						</li>
-						<li class="dropdown">
-							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#">Action</a>
-								</li>
-								<li>
-									<a href="#">Another action</a>
-								</li>
-								<li>
-									<a href="#">Something else here</a>
-								</li>
-								<li class="divider">
-								</li>
-								<li>
-									<a href="#">Separated link</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-				
-			</nav>
-			</div>
+	
 		<div>
 			<div class="carousel slide" id="carousel-179800">
 				<ol class="carousel-indicators">
@@ -94,43 +23,83 @@
 						<img alt="" src="{{asset('static/img/1.jpg')}}" />
 						<div class="carousel-caption">
 							<h4>
-								First Thumbnail label
+								<a href="" class="btn btn-block btn-danger pull-right">Servicio de Calidad a su alcance</a>
 							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							<p class="bg-primary">
+								Le ofrecemos un servicio de primera calidad, garantizamos nuestros trabajos<br>
+								y todo a un costo al alcance de su bolsillo.
 							</p>
 						</div>
 					</div>
 					<div class="item">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/2" />
+						<img alt="" src="{{asset('static/img/2.jpg')}}" />
 						<div class="carousel-caption">
 							<h4>
-								Second Thumbnail label
+								<a href="" class="btn btn-block btn-danger pull-right">Deteccion de errores de sistema y de hardware</a>
 							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							<p class="bg-primary">
+								Si esta cansado de la Pantallita azul del amigo Bill, somos su mejor opcion.<br>
+								Contamos con un equipo especializado en detectar esas maleficas fallas.
 							</p>
+							
 						</div>
 					</div>
 					<div class="item active">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/3" />
+						<img alt="" src="{{asset('static/img/3.jpg')}}" />
 						<div class="carousel-caption">
 							<h4>
-								Third Thumbnail label
+								<a href="" class="btn btn-block btn-danger pull-right">Equipo Tecnico capacitado</a>
 							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							<p class="bg-primary">
+								Nuestro equipo se capacita constantemente para estar preparado para los rapidos cambios de la tecnologia<br>
+								y de esta manera ofrecerle un servicio seguro y garantizado.
 							</p>
 						</div>
 					</div>
 				</div> <a class="left carousel-control" href="#carousel-179800" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-179800" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 			</div>
 		</div>
-		<div class="jumbotron">
+		<div class="bg-primary section">
+			<div class="container">
+				<br>
+			<div class="row ">
+			  <div class="col-sm-4 col-md-4">
+			    <div class="thumbnail">
+			      <img src="{{asset('static/img/4.jpg')}}" class="thumbnail">
+			      <div class="caption">
+			        <h3>Seguimiento on line</h3>
+			        <p>Consulte en linea el proceso de reparacion de su equipo.</p>
+			        <p><a href="#" class="btn btn-block btn-info" role="button">Consultar</a> </p>
+			      </div>
+			    </div>
+			  </div>
 			
+			
+			  <div class="col-sm-4 col-md-4">
+			    <div class="thumbnail">
+			      <img src="{{asset('static/img/5.jpg')}}" class="thumbnail">
+			      <div class="caption">
+			        <h3>Solicitud de servicio On line</h3>
+			        <p>Registrese en nuestro sitio para solicitar servicio para su equipo desde su casa.</p>
+			        <p><a href="#" class="btn btn-block btn-info" role="button">Registrarse</a> </p>
+			      </div>
+			    </div>
+			  </div>
+			
+			  <div class="col-sm-4 col-md-4">
+			    <div class="thumbnail">
+			      <img src="{{asset('static/img/6.jpg')}}" class="thumbnail">
+			      <div class="caption">
+			        <h3>Aplicacion m&oacute;vil</h3>
+			        <p>Reciba notificaciones sobre su equipo en servicio tecnico en su equipo movil.</p>
+			        <p><a href="#" class="btn btn-block btn-info" role="button">Descargar la aplicacion</a> </p>
+			      </div>
+			    </div>
+			  </div>
+			</div>
 		</div>
-		<div class="jumbotron" style="padding-bottom:0px">
-			
+		</div>
+		<div class="footer">
 		</div>
 </body>
 </html>
